37b92e64924aa4b32808b069c8ca344b42e1e3bf
Christian Westgaard <cwe@enonic.com>
2017-12-15 10:15:52 +0100

    Page with region

--- src/main/resources/site/pages/render-js/render-js.js
+++ src/main/resources/site/pages/render-js/render-js.js
@@ -1,16 +1,21 @@
 var R = require('/lib/render-js/index.js');
+var P = require('/lib/xp/portal');
 
 
 exports.get = function(request) {
+  var content = P.getContent();
   return {
     body: R.render([
       R.doctype(),
       R.html([
         R.head(R.title('Page Title')),
         R.body([
-          R.main([
-            R.h1('Hello World!')
-          ]) // main
+          R.main(
+            {dataPortalRegion: 'main'},
+            content.page.regions.main.components.map(function(c) {
+              '<!--# COMPONENT ' + c.path + ' -->'
+            }) // map
+          ) // main
         ]) // body
       ]) // html
     ]) // render
