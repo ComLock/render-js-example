fba760a0f7b87b3d787c133e21dc8fb186bb876c
Christian Westgaard <cwe@enonic.com>
2017-12-15 10:51:56 +0100

    Align and justify

---
+++ src/main/resources/site/layouts/grid/grid.js
@@ -0,0 +1,42 @@
+var R = require('/lib/render-js/index.js');
+var P = require('/lib/xp/portal');
+
+
+exports.get = function(request) {
+  var layoutComponent = P.getComponent();
+  // log.info('layoutComponent: ' + JSON.stringify(layoutComponent, null, 4));
+  var layoutHtml = R.div(
+    {
+      dataPortalComponentType: 'layout',
+      style: {
+        alignContent: layoutComponent.config.alignContent,
+        alignItems: layoutComponent.config.alignItems,
+        display: 'grid',
+        gridColumnGap: layoutComponent.config.gridColumnGap,
+        gridRowGap: layoutComponent.config.gridRowGap,
+        gridTemplateColumns: layoutComponent.config.gridTemplateColumns,
+        gridTemplateRows: layoutComponent.config.gridTemplateRows,
+        justifyContent: layoutComponent.config.justifyContent,
+        justifyItems: layoutComponent.config.justifyItems
+      }
+    },
+    Object.keys(layoutComponent.regions).map(function(regionKey) {
+      // log.info('regionKey: ' + JSON.stringify(regionKey, null, 4));
+      var region = layoutComponent.regions[regionKey];
+      // log.info('region: ' + JSON.stringify(region, null, 4));
+      return R.div(
+        {
+          dataPortalRegion: region.name,
+        },
+        region.components && region.components.map(function(component) {
+          return '<!--# COMPONENT ' + component.path + ' -->';
+        }) || '' // components map
+      ) // div
+    }) // regions map
+  ); // render
+  // log.info('layoutHtml: ' + JSON.stringify(layoutHtml, null, 4));
+  return {
+    body: layoutHtml,
+    contentType: 'text/html; charset=utf-8'
+  };
+};
--- src/main/resources/site/pages/render-js/render-js.js
+++ src/main/resources/site/pages/render-js/render-js.js
@@ -4,20 +4,23 @@ var P = require('/lib/xp/portal');
 
 exports.get = function(request) {
   var content = P.getContent();
+  var pageHtml = R.render([
+    R.doctype(),
+    R.html([
+      R.head(R.title('Page Title')),
+      R.body([
+        R.main(
+          {dataPortalRegion: 'main'},
+          content.page.regions.main.components && content.page.regions.main.components.map(function(component) {
+            return '<!--# COMPONENT ' + component.path + ' -->';
+          }) || '' // components map
+        ) // main
+      ]) // body
+    ]) // html
+  ]);
+  //log.info('pageHtml: ' + JSON.stringify(pageHtml, null, 4));
   return {
-    body: R.render([
-      R.doctype(),
-      R.html([
-        R.head(R.title('Page Title')),
-        R.body([
-          R.main(
-            {dataPortalRegion: 'main'},
-            content.page.regions.main.components.map(function(c) {
-              '<!--# COMPONENT ' + c.path + ' -->'
-            }) // map
-          ) // main
-        ]) // body
-      ]) // html
-    ]) // render
+    body: pageHtml,
+    contentType: 'text/html; charset=utf-8'
   }; // return
 } // exports.get
